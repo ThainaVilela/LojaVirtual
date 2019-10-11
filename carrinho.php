fb0110a7ac13428bb47d619413fa0ddb1b2a1415
Thainá Vilela <thainaferreiravilela@gmail.com>
2019-10-11 09:21:39 -0300

    Criando validacoes e mensagens de erro

--- carrinho.php
+++ carrinho.php
@@ -1,9 +1,3 @@
-        
-<?php
-$nomeSistema = "Fox Cursos";
-$usuario = ["nome"=>"Thainá"];
-$categorias = ["Cursos", "Palestras", "Artigos", "Blog"];
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,57 +9,32 @@ $categorias = ["Cursos", "Palestras", "Artigos", "Blog"];
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header class="navbar">
-        <h1 id="logo">
-            <?php echo $nomeSistema; ?>
-        </h1>
-        <nav>
-            <ul class="nav">
-                <?php if(isset($usuario) && $usuario !=[]) {?> 
-                    <li class="nav-item"><a class="nav-link" href="#">Cursos</a></li> 
-                    <li class="nav-item"><a class="nav-link" href="#">Olá <?php echo $usuario["nome"] ?>!</a></li>
-                <?php }else{ ?>
-                    <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Cadastrar</a></li>
-                <?php } ?>
-            </ul>
-        </nav>
-    </header>
-    <nav class="navbar navbar-dark bg-dark">
-        <ul class="nav">
-        <?php if(isset($categorias) && $categorias !=[]) {?>
-        <?php foreach($categorias as $categoria) { ?>
-        <li class="nav-item"><a class="nav-link text-white" href="#"><?php echo $categoria;?> </a> </li>
-        <?php } ?>
-        <?php } ?>
-        </ul>
-        </nav>
-        
+    <?php include_once("header.php"); ?>
     <main>
-    <section class="container">
-    <div class="row">
-    <div class="col-12">
-    <h1>Carrinho de compras</h1>
-    </div>
-    <div class="col-12">
-        <div class="row card">
-            <div class="col-12">
-                <h3>Você está comprando o curso  <?php echo $_GET['nomeProduto']; ?></h3>
+        <section class="container">
+            <div class="row">
+                <div class="col-12">
+                  <h1>Carrinho de compras</h1>
+                </div>
+                <div class="col-12">
+                    <div class="row card">
+                        <div class="col-12">
+                            <h3>Você está comprando o curso  <?php echo $_GET['nomeProduto']; ?></h3>
+                        </div>
+                    <div class="col-lg-6 col-md-6">
+                        <form class="d-flex flex-column p-3" method="post" action="sucesso.php">
+                            <input type="text" name="nomeCompleto" placeholder="Digite seu nome">
+                            <input type="text" name="cpf" placeholder="CPF">
+                            <input type="number" name="cartao" placeholder="número do cartão">
+                            <input type="date" name="validadeCartao" placeholder="Data de validade">
+                            <input type="number" name="codigoCartao" placeholder="CVV">
+                            <button class="btn btn-sucess" type="submit"> Finalizar a Compra</button>
+                        </form>
+                    </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-6 col-md-6">
-                <form class="d-flex flex-column p-3" method="post" action="sucesso.php">
-                <input type="text" name="nomeCompleto" placeholder="Digite seu nome">
-                <input type="text" name="cpf" placeholder="CPF">
-                <input type="number" name="cartao" placeholder="número do cartão">
-                <input type="date" name="validadeCartao" placeholder="Data de validade">
-                <input type="number" name="codigoCartao" placeholder="CVV">
-                 <button class="btn btn-sucess" type="submit"> Finalizar a Compra</button>
-                </form>
-            </div>
-        </div>
-    </div>
-    </div>
-    </section>
+        </section>
     </main>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-        
-<?php
-$nomeSistema = "Fox Cursos";
-$usuario = ["nome"=>"Thainá"];
-
-$produtos = [
-     ["nome"=>"Curso HTML", "preco"=>59.00, "duracao"=>"1 mes","img"=>"img/html.png"],
-     ["nome"=>"Curso JavaScript","preco"=>59.00, "duracao"=>"4 meses","img"=>"img/js.png"],
-     ["nome"=>"Curso CSS","preco"=>59.00, "duracao"=>"2 meses","img"=>"img/css.png"],
-];
-
-$categorias = ["Cursos", "Palestras", "Artigos", "Blog"];
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,39 +9,9 @@ $categorias = ["Cursos", "Palestras", "Artigos", "Blog"];
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header class="navbar">
-        <h1 id="logo">
-            <?php echo $nomeSistema; ?>
-        </h1>
-        <nav>
-            <ul class="nav">
-                <?php if(isset($usuario) && $usuario !=[]) {?> 
-                    <li class="nav-item"><a class="nav-link" href="#">Cursos</a></li> 
-                    <li class="nav-item"><a class="nav-link" href="#">Olá <?php echo $usuario["nome"] ?>!</a></li>
-                <?php }else{ ?>
-                    <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Cadastrar</a></li>
-                <?php } ?>
-            </ul>
-        </nav>
-    </header>
-    <nav class="navbar navbar-dark bg-dark">
-        <ul class="nav">
-        <?php if(isset($categorias) && $categorias !=[]) {?>
-        <?php foreach($categorias as $categoria) { ?>
-        <li class="nav-item"><a class="nav-link text-white" href="#"><?php echo $categoria;?> </a> </li>
-        <?php } ?>
-        <?php } ?>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-        </nav>
-        
+<?php include_once("header.php"); ?>
     <main>
     <section class="container mt-4">
-
         <div class="row justify-content-around">
         <?php if(isset($produtos) && $produtos != []){ ?>
            <?php foreach($produtos as $produto) { ?>
