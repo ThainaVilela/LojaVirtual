ba22646a96f03c3592df14f0ce8579a0fd567c32
Thaina Vilela <thainaferreiravilela@gmail.com>
2019-10-16 12:07:54 -0300

    Criando pagina de login de usuário com validação, criando sessão

--- variaveis.php
+++ variaveis.php
@@ -1,14 +1,16 @@
 <?php 
-    $nomeSistema = "Fox Cursos";
-    $usuario = ["nome"=>"Thaina"];
+    session_start();
+        $nomeSistema = "Fox Cursos";
+        //Definindo usuário- se existir a sessão usuário(?) se existir o valor dela será o que já foi recebido e senão ficará vazio ("")
+        $usuario = isset($_SESSION['usuario'])? $_SESSION['usuario']: [];
 
-    $nomeArquivo = "produto.json";
-    $produtos = json_decode(file_get_contents($nomeArquivo), true);;
-    
-    // $produtos = [
-    //     ["nome"=>"CSS","preco"=>1200.00,"duracao"=>"5 meses","img"=>"img/css.png"],
-    //     ["nome"=>"HTML","preco"=>1000.00,"duracao"=>"4 meses","img"=>"img/html.png"],
-    //     ["nome"=>"JS","preco"=>1000.00,"duracao"=>"4 meses","img"=>"img/js.png"],
-    // ];
-    $categorias = ["Cursos","Palestras","Artigos","Blog"];
+        $nomeArquivo = "produto.json";
+        $produtos = json_decode(file_get_contents($nomeArquivo), true);;
+        
+        // $produtos = [
+        //     ["nome"=>"CSS","preco"=>1200.00,"duracao"=>"5 meses","img"=>"img/css.png"],
+        //     ["nome"=>"HTML","preco"=>1000.00,"duracao"=>"4 meses","img"=>"img/html.png"],
+        //     ["nome"=>"JS","preco"=>1000.00,"duracao"=>"4 meses","img"=>"img/js.png"],
+        // ];
+        $categorias = ["Cursos","Palestras","Artigos","Blog"];
 ?>
